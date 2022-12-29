5a520baffc8323938334892e01df76b95f275258
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-29 11:18:17 +0100

    feat(Pictures): :sparkles: Display all pictures in a list

--- screens/Pictures.js
+++ screens/Pictures.js
@@ -1,19 +1,48 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  FlatList,
+  SafeAreaView,
+} from "react-native";
 import { connect } from "react-redux";
 
+const Item = ({ title, uri }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Image style={styles.image} source={{ uri: uri }} />
+  </View>
+);
+
 const Pictures = (props) => {
-  return (
-    <View style={styles.mainContent}>
-      {props.images && (
-        <View>
-          <Image source={{ uri: props.images[0].data }} style={styles.image} />
-          <Text>{props.images[0].date}</Text>
-          <Text>{props.images[0].id}</Text>
-        </View>
-      )}
-    </View>
-  );
+  if (props.images && props.images.length > 0) {
+    const renderItem = ({ item }) => <Item title={item.date} uri={item.uri} />;
+    const DATA = props.images.map((image) => {
+      return {
+        id: image.id,
+        date: image.date,
+        uri: image.data,
+      };
+    });
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <View style={styles.mainContent}>
+        <Text>No images to display</Text>
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
