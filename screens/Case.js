7e0dff89c64b01d42a46dacf6354b9554295769d
AntoinePiron <pro.piron.antoine@gmail.com>
2022-12-31 13:18:51 +0100

    feat(Case): :sparkles: automatic generate of case form

--- screens/Case.js
+++ screens/Case.js
@@ -1,31 +1,54 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import { StyleSheet, View, SafeAreaView, FlatList } from "react-native";
 import ScanInput from "../components/BasicUI/ScanInput";
 
+const FORM = [
+  {
+    key: "forname",
+    placeholder: "Forename",
+    value: "",
+    onChangeText: null,
+    keyboardType: "default",
+  },
+  {
+    key: "lastname",
+    placeholder: "Lastname",
+    value: "",
+    onChangeText: null,
+    keyboardType: "default",
+  },
+  {
+    key: "age",
+    placeholder: "Age",
+    value: "",
+    onChangeText: null,
+    keyboardType: "numeric",
+  },
+];
+
 const Case = () => {
-  const [forename, onChangeForename] = useState("");
-  const [lastname, onChangeLastname] = useState("");
-  const [age, onChangeAge] = useState("");
+  FORM.forEach((element) => {
+    const [value, onChangeText] = useState(element.value);
+    element.value = value;
+    element.onChangeText = onChangeText;
+  });
+  const renderItem = ({ item }) => (
+    <ScanInput
+      placeholder={item.placeholder}
+      value={item.value}
+      onChangeText={item.onChangeText}
+      keyboardType={item.keyboardType}
+    />
+  );
 
   return (
-    <View style={styles.mainContent}>
-      <ScanInput
-        placeholder="Forename"
-        value={forename}
-        onChangeText={onChangeForename}
+    <SafeAreaView style={styles.mainContent}>
+      <FlatList
+        data={FORM}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.key}
       />
-      <ScanInput
-        placeholder="Lastname"
-        value={lastname}
-        onChangeText={onChangeLastname}
-      />
-      <ScanInput
-        placeholder="Age"
-        value={age}
-        onChangeText={onChangeAge}
-        keyboardType="numeric"
-      />
-    </View>
+    </SafeAreaView>
   );
 };
 
