6438842f73a19d8869d2016768de6f4ce800c6c8
MattPavel07 <nsielamatthieu@gmail.com>
2024-05-29 09:47:32 +0200

    Refactor modification de la fonction funeraille.js

--- screens/Funeraille.js
+++ screens/Funeraille.js
@@ -35,114 +35,20 @@ const Funeraille = (props) => {
   const [images, setImages] = useState([]);
   const dispatch = useDispatch();
 
-  // const FORM = [
-  //   {
-  //     key: "forname",
-  //     placeholder: intlData.messages.Case.forename,
-  //     value: "",
-  //     onChangeText: null,
-  //     keyboardType: "default",
-  //   },
-  //   {
-  //     key: "lastname",
-  //     placeholder: intlData.messages.Case.lastname,
-  //     value: "",
-  //     onChangeText: null,
-  //     keyboardType: "default",
-  //   },
-  //   {
-  //     key: "age",
-  //     placeholder: intlData.messages.Case.age,
-  //     value: "",
-  //     onChangeText: null,
-  //     keyboardType: "numeric",
-  //   },
-  //   {
-  //     key: "injuries",
-  //     placeholder: intlData.messages.Case.injury,
-  //     value: "",
-  //     onChangeText: null,
-  //     keyboardType: "default",
-  //   },
-  // ];
-
-  // const isCaseEmpty = () => {
-  //   return FORM.every((element) => element.value === "");
-  // };
-
-  //Change the back button onpress beahviour and disable swipe back
-  // useLayoutEffect(() => {
-  //   navigation.setOptions({
-  //     headerLeft: (props) => (
-  //       <HeaderBackButton
-  //         {...props}
-  //         onPress={() => {
-  //           if (!existingCase && !isCaseEmpty()) {
-  //             Alert.alert(intlData.messages.Case.confirmBack, "", [
-  //               {
-  //                 text: intlData.messages.yes,
-  //                 onPress: () => {
-  //                   dispatch(deleteCase(caseID));
-  //                   if (images.length > 0) {
-  //                     images.forEach((image) => {
-  //                       deleteImageFromMemory(image.id);
-  //                     });
-  //                   }
-  //                   deleteCameraCache();
-  //                   navigation.goBack();
-  //                 },
-  //               },
-  //               {
-  //                 text: intlData.messages.no,
-  //               },
-  //             ]);
-  //           } else {
-  //             navigation.goBack();
-  //           }
-  //         }}
-  //       />
-  //     ),
-  //     gestureEnabled: false,
-  //   });
-  // }, [navigation, FORM, images]);
-
-  // FORM.forEach((element) => {
-  //   const [value, onChangeText] = useState(element.value);
-  //   element.value = value;
-  //   element.onChangeText = onChangeText;
-  // });
-
   const isCaseComplete = () => {
     if (images.length === 0) {
       alert(intlData.messages.Case.addImage);
       return false;
     }
-    // const keyValues = FORM.map((element) => {
-    //   return { [element.key]: element.value };
-    // });
-    //if a value is empty send an alert
-    // const emptyValues = keyValues.filter((element) => {
-    //   return element[Object.keys(element)[0]] === "";
-    // });
-    // if (emptyValues.length > 0) {
-    //   alert(intlData.messages.Case.allFields);
-    //   return false;
-    // }
-    else 
-    {return true};
+    return true;
   };
 
   const save = () => {
     if (!isCaseComplete()) return;
-    // const keyValues = FORM.map((element) => {
-    //   return { [element.key]: element.value };
-    // });
-    // const keyValuesObject = Object.assign({}, ...keyValues);
     dispatch(setIdentifier("dead"));
     const imageIDs = images.map((image) => image.id);
     const data = {
       id: caseID,
-      // ...keyValuesObject,
       images: imageIDs,
       date: new Date().toISOString(),
     };
@@ -158,30 +64,22 @@ const Funeraille = (props) => {
 
   const submit = async () => {
     if (!isCaseComplete()) return;
-    // const keyValues = FORM.map((element) => {
-    //   return { [element.key]: element.value };
-    // });
-    // const keyValuesObject = Object.assign({}, ...keyValues);
     dispatch(setIdentifier("dead"));
     const imageIDs = images.map((image) => image.id);
-    console.log(identifier)
     const coordinates = images.map((image) => {
       return {
         id: image.id,
         latitude: image.lat,
         longitude: image.lng,
       };
-      
     });
     const data = {
       id: caseID,
-      // ...keyValuesObject,
       images: imageIDs,
       date: new Date().toISOString(),
       coordinates: coordinates,
     };
     const path = await createZip(data);
-    //Check if mail is available
     const isAvailable = await MailComposer.isAvailableAsync();
     if (!isAvailable) {
       alert(intlData.messages.Case.noMail);
@@ -209,40 +107,19 @@ const Funeraille = (props) => {
 
   useEffect(() => {
     if (props.route.params && props.route.params.caseId) {
-      const mcase = props.cases.filter(
+      const mcase = props.cases.find(
         (item) => item.id === props.route.params.caseId
-      )[0];
+      );
       setExistingCase(mcase);
       setCaseID(mcase.id);
     } else {
       setCaseID(uuid.v4());
     }
-  }, [caseID, props.image]);
-
-  //   return () => {
-  //     //clear form
-  //   //   FORM.forEach((element) => {
-  //   //     element.value = "";
-  //   //     element.onChangeText("");
-  //   //   });
-  //   // };
-  // }, 
-    // }, []);
-        
-
-  // useEffect(() => {
-  //   //update form with existing case if it exist
-  //   if (existingCase) {
-  //     FORM.forEach((element) => {
-  //       element.onChangeText(existingCase[element.key]);
-  //     });
-  //   }
-  //   setCaseImages();
-  // }, [existingCase]);
+  }, [props.route.params]);
 
   useEffect(() => {
     setCaseImages();
-  }, []);
+  }, [props.images, caseID]); 
 
   const renderItem = ({ item }) => (
     <ScanInput
@@ -252,6 +129,7 @@ const Funeraille = (props) => {
       keyboardType={item.keyboardType}
     />
   );
+
   const renderImage = ({ item }) => (
     <Pressable
       onPress={() => navigation.navigate("Pictures", { caseID: item.caseID })}
@@ -271,38 +149,36 @@ const Funeraille = (props) => {
       onPress={() => Keyboard.dismiss()}
     >
       <View style={styles.pictureButton}>
-      <Image 
-      onPress={() => navigation.navigate("Camera", { caseID: caseID })}
-      activeOpacity={1}
-      source={myImage} style={styles.image} />
-      <ScanButton
-        title={intlData.messages.Case.photoButton}
-        onPress={() => navigation.navigate("Camera", { caseID: caseID })}
-      />
-      <Text style={{ fontStyle: "italic" }}>
-        {intlData.messages.Case.descriptionPhoto}
-      </Text>
-      <FlatList
-        data={images}
-        renderItem={renderImage}
-        keyExtractor={(item) => item.id}
-        ListFooterComponent={<View style={{ height: 50 }} />}
-        style={{ flexGrow: 0 }}
-        horizontal={true}
-      />
+        <Image 
+          onPress={() => navigation.navigate("Camera", { caseID: caseID })}
+          activeOpacity={1}
+          source={myImage} 
+          style={styles.image} 
+        />
+        <ScanButton
+          title={intlData.messages.Case.photoButton}
+          onPress={() => navigation.navigate("Camera", { caseID: caseID })}
+        />
+        <Text style={{ fontStyle: "italic" }}>
+          {intlData.messages.Case.descriptionPhoto}
+        </Text>
+        <FlatList
+          data={images}
+          renderItem={renderImage}
+          keyExtractor={(item) => item.id}
+          ListFooterComponent={<View style={{ height: 50 }} />}
+          style={{ flexGrow: 0 }}
+          horizontal={true}
+        />
       </View>
       <View style={styles.button}>
         <LittleScanButton
           title={intlData.messages.Case.saveButton}
-          onPress={() => {
-            save();
-          }}
+          onPress={save}
         />
         <LittleScanButton
           title={intlData.messages.Case.submitButton}
-          onPress={() => {
-            submit();
-          }}
+          onPress={submit}
         />
       </View>
     </Pressable>
@@ -314,20 +190,16 @@ const basicStyle = StyleSheet.create({
     flex: 1,
     alignItems: "center",
   },
-
   pictureButton: {
-    width: '60%',  // Adjust the width as needed
+    width: '60%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    
   },
-
   image: {
-    width: '60%',  // Adjust the width as needed
+    width: '60%',
     height: '20%',
   },
-
   button: {
     position: "absolute",
     bottom: 0,
@@ -335,7 +207,6 @@ const basicStyle = StyleSheet.create({
     justifyContent: "space-around",
     width: "100%",
   },
-  
   activityContainer: {
     position: "absolute",
     top: 0,
