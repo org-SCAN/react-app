871070371a550b233719e5ed7822b17ba070b55c
Antoine Bois--Berlioz <antoine.boisberlioz@gmail.com>
2025-01-10 04:20:13 +0100

    feat: add function to delete icons from local storage and fix errors from previous merge

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -34,6 +34,14 @@ export async function deleteAll() {
   );
 }
 
+export async function deleteIcons() {
+  await FileSystem.deleteAsync(FileSystem.documentDirectory + "icons").catch(
+    (err) => {
+      console.log(err);
+    }
+  );
+}
+
 export async function createZip(caseData) {
   const zip = new JSZip();
   const images = caseData.images;
@@ -132,54 +140,5 @@ export async function deleteImageCase(caseData) {
   for (let i = 0; i < images.length; i++) {
     const image = images[i];
     await deleteImageFromMemory(image);
-  }
-}
-
-
-export async function openZipAndExtractIcons(zipPath) {
-  try {
-    const zipContent = await FileSystem.readAsStringAsync(zipPath, {
-      encoding: FileSystem.EncodingType.Base64,
-    });
-    const zip = new JSZip();
-    const zipData = await zip.loadAsync(zipContent, { base64: true });
-    const iconsFolder = FileSystem.documentDirectory + "icons/";
-    await FileSystem.makeDirectoryAsync(iconsFolder, { intermediates: true });
-    // Extract EACH ICON file
-    for (const fileName of Object.keys(zipData.files)) {
-      const file = zipData.files[fileName];
-      // to do - trouver un standart pour les icons 
-      if (fileName.match(/\.(png|jpg|jpeg|svg)$/i)) {
-        const fileData = await file.async("base64"); 
-        const filePath = iconsFolder + fileName;
-        // Save the icon
-        await FileSystem.writeAsStringAsync(filePath, fileData, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        console.log(`Extracted icon: ${filePath}`);
-      } else {
-        console.log(`Skipped non-icon file: ${fileName}`);
-      }
-    }
-    console.log("Icons extraction completed:", iconsFolder);
-    return iconsFolder;
-  } catch (error) {
-    console.error("Error extracting icons from ZIP file:", error);
-    throw error;
-  }
-}
-
-export async function downloadZipFile(zipUrl) { 
-  const destinationDir = FileSystem.documentDirectory + "zip"; 
-  const destinationPath = destinationDir + "/icons.zip"; 
-  try {
-    await FileSystem.makeDirectoryAsync(destinationDir, { intermediates: true });
-    console.log("Directory created or already exists:", destinationDir);
-    console.log("Starting download...");
-    const downloadResult = await FileSystem.downloadAsync(zipUrl, destinationPath);
-    console.log("File downloaded to:", downloadResult.uri);
-    
-  } catch (error) {
-    console.error("Failed to download ZIP file:", error);
   }
 }
